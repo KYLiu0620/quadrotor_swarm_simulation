2c16457a506b045c080e0ad132214e7d70b74658
jackma-ME <jackma010039@gmail.com>
2018-10-29 17:03:42 +0800

    [update]six quadrotors & scripts

---
+++ scripts/crazyflie_exp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+roslaunch crazyflie_demo hover_vrpn.launch ip:=192.168.50.21 frame:=CF1 uri:=radio://0/80/250K
---
+++ scripts/enable_motors.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rosservice call /robot1/enable_motors True
+rosservice call /robot2/enable_motors True
+rosservice call /robot3/enable_motors True
+rosservice call /robot4/enable_motors True
+rosservice call /robot5/enable_motors True
+rosservice call /robot6/enable_motors True
+
+
